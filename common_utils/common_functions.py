0baaa1fda8193b505b3f6ec5d07d64ade6fa9e26
parth.mundhwa <parth.mundhwa@inferenz.ai>
2023-04-25 11:41:07 +0530

    Changes 25-04-2023: Dataframes merged and created code to store data into the local resource directory.

--- common_utils/common_functions.py
+++ common_utils/common_functions.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
+
 ROOT_DIR = os.path.dirname(os.path.abspath(path="./"))
 
 
-
-
+def merge_df(fdf, first_key, sdf, skey):
+    df = fdf.merge(sdf, how='left', left_on=first_key, right_on=skey)
+    return df
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os
 import pandas as pd
+
+from common_utils.common_functions import merge_df
+
 ROOT_DIR = os.path.dirname(os.path.abspath(path="Airlines_2020"))
 
 active_weather_df = pd.read_csv(ROOT_DIR + "/resources/ActiveWeather.csv")
 cancellation_df = pd.read_csv(ROOT_DIR + "/resources/Cancellation.csv")
-carriers_df = pd.read_csv(ROOT_DIR + "/resources/Carriers.csv" )
+carriers_df = pd.read_csv(ROOT_DIR + "/resources/Carriers.csv")
 complete_data_df = pd.read_csv(ROOT_DIR + "/resources/CompleteData.csv")
-station_df = pd.read_csv(ROOT_DIR + "/resources/Stations.csv")
+
+
+cancelled_merged_df = merge_df(complete_data_df, "CANCELLED", cancellation_df, "STATUS")
+active_weather_merged_df = merge_df(complete_data_df, "ACTIVE_WEATHER", active_weather_df, "STATUS")
+carriers_merged_df = merge_df(complete_data_df, "OP_UNIQUE_CARRIER", carriers_df, "CODE")
+
+cancelled_merged_df.to_csv(ROOT_DIR + "/resources/cancelled_merge_df.csv",index=False)
+active_weather_merged_df.to_csv(ROOT_DIR + "/resources/active_weather_merged.csv",index=False)
+carriers_merged_df.to_csv(ROOT_DIR + "/resources/carriers_merged.csv",index=False)
 
